9bf22f5ecacada99e7977f30911706d21d7807c7
chilipp89 <pjunietz@gmail.com>
2022-12-07 00:26:08 +0100

    buttons

--- pibooth_fire_remote.py
+++ pibooth_fire_remote.py
@@ -8,15 +8,17 @@ gamepad = InputDevice('/dev/input/event2')
 print(gamepad)
 
 #button code variables (change to suit your device)
-enterBtn = 304
-MicroBtn = 305
-up = 115
-down = 114
-left = 165
-right = 163
+enterBtn = 96
+backBtn = 158
+settingsBtn = 139
+homeBtn = 172
+up = 103
+down = 108
+left = 105
+right = 106
 playpause = 164
-forward = 166
-backward = 167
+forward = 208
+backward = 168
 
 #loop and filter by event code and print the mapped label
 for event in gamepad.read_loop():
@@ -26,8 +28,12 @@ for event in gamepad.read_loop():
         if event.value == 1:
             if event.code == enterBtn:
                 print("enterBtn")
-            elif event.code == MicroBtn:
-                print("MicroBtn")
+            elif event.code == settingsBtn:
+                print("settingsBtn")
+            if event.code == homeBtn:
+                print("homeBtn")
+            elif event.code == backBtn:
+                print("backBtn")
             elif event.code == playpause:
                 print("Play/Pause")
             elif event.code == up:
