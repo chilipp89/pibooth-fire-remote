162dbf7449241f8e909d15d0321470c98f5511ed
chilipp89 <pjunietz@gmail.com>
2022-12-07 21:43:07 +0100

    test

--- pibooth_fire_remote.py
+++ pibooth_fire_remote.py
@@ -1,13 +1,14 @@
 from evdev import InputDevice, categorize, ecodes
 import pygame
 
+import asyncio
 import pibooth
 from pibooth.utils import LOGGER
 
 LOGGER.info("Fire Remote Gen 1 - mapping")
 
 #creates object 'gamepad' to store the data
-gamepad = InputDevice('/dev/input/event2')
+dev = InputDevice('/dev/input/event2')
 
 BUTTONDOWN = pygame.USEREVENT + 1
 
@@ -24,35 +25,41 @@ playpause = 164
 forward = 208
 backward = 168
 
+async def helper(dev):
+    async for ev in dev.async_read_loop():
+        print(repr(ev))
+
 @pibooth.hookimpl
 def pibooth_startup(app):
-    #loop and filter by event code and print the mapped label
-    for event in gamepad.read_loop():
-
-        if event.type == ecodes.EV_KEY:
-            if event.value == 1:
-                if event.code == enterBtn:
-                    event = pygame.event.Event(BUTTONDOWN, capture=1, printer=0,
-                                               button=app.buttons.capture)
-
-                    LOGGER.info("enterBtn")
-                elif event.code == settingsBtn:
-                    LOGGER.info("settingsBtn")
-                if event.code == homeBtn:
-                    LOGGER.info("homeBtn")
-                elif event.code == backBtn:
-                    LOGGER.info("backBtn")
-                elif event.code == playpause:
-                    LOGGER.info("Play/Pause")
-                elif event.code == up:
-                    LOGGER.info("up")
-                elif event.code == down:
-                    LOGGER.info("down")
-                elif event.code == left:
-                    LOGGER.info("left")
-                elif event.code == right:
-                    LOGGER.info("right")
-                elif event.code == forward:
-                    LOGGER.info("forward")
-                elif event.code == backward:
-                    LOGGER.info("backward")
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(helper(dev))
+    # #loop and filter by event code and print the mapped label
+    # for event in gamepad.read_loop():
+    #
+    #     if event.type == ecodes.EV_KEY:
+    #         if event.value == 1:
+    #             if event.code == enterBtn:
+    #                 event = pygame.event.Event(BUTTONDOWN, capture=1, printer=0,
+    #                                            button=app.buttons.capture)
+    #
+    #                 LOGGER.info("enterBtn")
+    #             elif event.code == settingsBtn:
+    #                 LOGGER.info("settingsBtn")
+    #             if event.code == homeBtn:
+    #                 LOGGER.info("homeBtn")
+    #             elif event.code == backBtn:
+    #                 LOGGER.info("backBtn")
+    #             elif event.code == playpause:
+    #                 LOGGER.info("Play/Pause")
+    #             elif event.code == up:
+    #                 LOGGER.info("up")
+    #             elif event.code == down:
+    #                 LOGGER.info("down")
+    #             elif event.code == left:
+    #                 LOGGER.info("left")
+    #             elif event.code == right:
+    #                 LOGGER.info("right")
+    #             elif event.code == forward:
+    #                 LOGGER.info("forward")
+    #             elif event.code == backward:
+    #                 LOGGER.info("backward")
