fef7a27d96e7467088d3a1cc0379b6ec7f77ec84
chilipp89 <pjunietz@gmail.com>
2022-12-07 21:34:57 +0100

    test

--- pibooth_fire_remote.py
+++ pibooth_fire_remote.py
@@ -1,11 +1,15 @@
 from evdev import InputDevice, categorize, ecodes
+import pygame
 
-print("ACGAM R1 - pad mapping")
+import pibooth
+from pibooth.utils import LOGGER
+
+LOGGER.info("Fire Remote Gen 1 - mapping")
 
 #creates object 'gamepad' to store the data
 gamepad = InputDevice('/dev/input/event2')
 
-print(gamepad)
+BUTTONDOWN = pygame.USEREVENT + 1
 
 #button code variables (change to suit your device)
 enterBtn = 96
@@ -20,31 +24,35 @@ playpause = 164
 forward = 208
 backward = 168
 
-#loop and filter by event code and print the mapped label
-for event in gamepad.read_loop():
-    print(categorize(event))
+@pibooth.hookimpl
+def pibooth_startup(app):
+    #loop and filter by event code and print the mapped label
+    for event in gamepad.read_loop():
 
-    if event.type == ecodes.EV_KEY:
-        if event.value == 1:
-            if event.code == enterBtn:
-                print("enterBtn")
-            elif event.code == settingsBtn:
-                print("settingsBtn")
-            if event.code == homeBtn:
-                print("homeBtn")
-            elif event.code == backBtn:
-                print("backBtn")
-            elif event.code == playpause:
-                print("Play/Pause")
-            elif event.code == up:
-                print("up")
-            elif event.code == down:
-                print("down")
-            elif event.code == left:
-                print("left")
-            elif event.code == right:
-                print("right")
-            elif event.code == forward:
-                print("forward")
-            elif event.code == backward:
-                print("backward")
+        if event.type == ecodes.EV_KEY:
+            if event.value == 1:
+                if event.code == enterBtn:
+                    event = pygame.event.Event(BUTTONDOWN, capture=1, printer=0,
+                                               button=app.buttons.capture)
+
+                    LOGGER.info("enterBtn")
+                elif event.code == settingsBtn:
+                    LOGGER.info("settingsBtn")
+                if event.code == homeBtn:
+                    LOGGER.info("homeBtn")
+                elif event.code == backBtn:
+                    LOGGER.info("backBtn")
+                elif event.code == playpause:
+                    LOGGER.info("Play/Pause")
+                elif event.code == up:
+                    LOGGER.info("up")
+                elif event.code == down:
+                    LOGGER.info("down")
+                elif event.code == left:
+                    LOGGER.info("left")
+                elif event.code == right:
+                    LOGGER.info("right")
+                elif event.code == forward:
+                    LOGGER.info("forward")
+                elif event.code == backward:
+                    LOGGER.info("backward")
