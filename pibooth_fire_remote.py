273539fd8b9d031864a1ef80ae4ecbe75b4996e7
chilipp89 <pjunietz@gmail.com>
2023-07-24 14:37:14 +0200

    fix sleeping device problem

--- pibooth_fire_remote.py
+++ pibooth_fire_remote.py
@@ -1,3 +1,4 @@
+import logging
 import threading
 
 from evdev import InputDevice, categorize, ecodes
@@ -11,7 +12,7 @@ import pibooth
 from pibooth.utils import LOGGER
 
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 
 
@@ -69,59 +70,63 @@ def pibooth_startup(cfg, app):
 
 def run_event_monitor(cfg, app):
     #loop and filter by event code and print the mapped label
-    dev = get_device(cfg)
-    if dev is not None:
+    while True:
 
-        pictureBtn = int(cfg.get(SECTION, 'pictureBtn'))
-        # backBtn = int(cfg.get(SECTION, 'backBtn'))
-        settingsBtn = int(cfg.get(SECTION, 'settingsBtn'))
-        # homeBtn = int(cfg.get(SECTION, 'homeBtn'))
-        # up = int(cfg.get(SECTION, 'up'))
-        # down = int(cfg.get(SECTION, 'down'))
-        left = int(cfg.get(SECTION, 'left'))
-        right = int(cfg.get(SECTION, 'right'))
-        # playpause = int(cfg.get(SECTION, 'playpause'))
-        printBtn = int(cfg.get(SECTION, 'printBtn'))
-        # backward = int(cfg.get(SECTION, 'backward'))
+        dev = get_device(cfg)
+        if dev is not None:
 
-        for event in dev.read_loop():
+            pictureBtn = int(cfg.get(SECTION, 'pictureBtn'))
+            # backBtn = int(cfg.get(SECTION, 'backBtn'))
+            settingsBtn = int(cfg.get(SECTION, 'settingsBtn'))
+            # homeBtn = int(cfg.get(SECTION, 'homeBtn'))
+            # up = int(cfg.get(SECTION, 'up'))
+            # down = int(cfg.get(SECTION, 'down'))
+            left = int(cfg.get(SECTION, 'left'))
+            right = int(cfg.get(SECTION, 'right'))
+            # playpause = int(cfg.get(SECTION, 'playpause'))
+            printBtn = int(cfg.get(SECTION, 'printBtn'))
+            # backward = int(cfg.get(SECTION, 'backward'))
+            try:
+                for event in dev.read_loop():
 
-            if event.type == ecodes.EV_KEY and hasattr(event, "code"):
-                if event.value == 1:
-                    if event.code == pictureBtn:
-                        pygame.event.post(pygame.event.Event(BUTTONDOWN, capture=1, printer=0,
-                                                   button=app.buttons.capture))
+                    if event.type == ecodes.EV_KEY and hasattr(event, "code"):
+                        if event.value == 1:
+                            if event.code == pictureBtn:
+                                pygame.event.post(pygame.event.Event(BUTTONDOWN, capture=1, printer=0,
+                                                           button=app.buttons.capture))
 
-                        LOGGER.info("pictureBtn")
-                    elif event.code == settingsBtn:
-                        pygame.event.post(pygame.event.Event(BUTTONDOWN, capture=1, printer=1,
-                                                             button=app.buttons))
-                        LOGGER.info("settingsBtn")
-                    # if event.code == homeBtn:
-                    #     LOGGER.info("homeBtn")
-                    # elif event.code == backBtn:
-                    #     LOGGER.info("backBtn")
-                    # elif event.code == playpause:
-                    #     LOGGER.info("Play/Pause")
-                    # elif event.code == up:
-                    #     keyboard.press(Key.up)
-                    #     keyboard.release(Key.up)
-                    #     LOGGER.info("up")
-                    # elif event.code == down:
-                    #     keyboard.press(Key.up)
-                    #     keyboard.release(Key.up)
-                    #     LOGGER.info("down")
-                    elif event.code == left:
-                        keyboard.press(Key.left)
-                        keyboard.release(Key.left)
-                        LOGGER.info("left")
-                    elif event.code == right:
-                        keyboard.press(Key.right)
-                        keyboard.release(Key.right)
-                        LOGGER.info("right")
-                    elif event.code == printBtn:
-                        pygame.event.post(pygame.event.Event(BUTTONDOWN, capture=0, printer=1,
-                                                             button=app.buttons.printer))
-                        LOGGER.info("printBtn")
-                    # elif event.code == backward:
-                    #     LOGGER.info("backward")
+                                LOGGER.info("pictureBtn")
+                            elif event.code == settingsBtn:
+                                pygame.event.post(pygame.event.Event(BUTTONDOWN, capture=1, printer=1,
+                                                                     button=app.buttons))
+                                LOGGER.info("settingsBtn")
+                            # if event.code == homeBtn:
+                            #     LOGGER.info("homeBtn")
+                            # elif event.code == backBtn:
+                            #     LOGGER.info("backBtn")
+                            # elif event.code == playpause:
+                            #     LOGGER.info("Play/Pause")
+                            # elif event.code == up:
+                            #     keyboard.press(Key.up)
+                            #     keyboard.release(Key.up)
+                            #     LOGGER.info("up")
+                            # elif event.code == down:
+                            #     keyboard.press(Key.up)
+                            #     keyboard.release(Key.up)
+                            #     LOGGER.info("down")
+                            elif event.code == left:
+                                keyboard.press(Key.left)
+                                keyboard.release(Key.left)
+                                LOGGER.info("left")
+                            elif event.code == right:
+                                keyboard.press(Key.right)
+                                keyboard.release(Key.right)
+                                LOGGER.info("right")
+                            elif event.code == printBtn:
+                                pygame.event.post(pygame.event.Event(BUTTONDOWN, capture=0, printer=1,
+                                                                     button=app.buttons.printer))
+                                LOGGER.info("printBtn")
+                            # elif event.code == backward:
+                            #     LOGGER.info("backward")
+            except OSError as err:
+                logging.WARNING(err)
